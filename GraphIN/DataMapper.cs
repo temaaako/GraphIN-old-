8593136fd1a9a40b365b682abbb7ac835e43b9e9
Artem <artem200110@gmail.com>
2023-04-11 21:37:21 +0300

    Первый коммит. Подключен live charts, выводится увеличенное значение раз в какое-то время.

---
+++ GraphIN/DataMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphIN
+{
+    public class DataMapper
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public DataMapper() { }
+        public DataMapper(float x, float y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
---
+++ GraphIN/MainVM.cs
@@ -0,0 +1,61 @@
+﻿using LiveCharts;
+using LiveCharts.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GraphIN
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ChartValues<MeasureModel> Values { get; set; }
+
+
+        public MainVM() {
+            //used to generate random values
+            var random = new Random();
+            var time = 0d;
+
+            //lets instead plot elapsed milliseconds and value
+            var mapper = Mappers.Xy<MeasureModel>()
+                .X(x => x.ElapsedMilliseconds)
+                .Y(x => x.Value);
+            
+
+            //save the mapper globally         
+            Charting.For<MeasureModel>(mapper);
+
+            Values = new ChartValues<MeasureModel>();
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    //we add the lecture based on our StopWatch instance
+                    Values.Add(new MeasureModel
+                    {
+                        ElapsedMilliseconds = sw.ElapsedMilliseconds,
+                        Value = time += random.Next(0, 10)
+                    });
+                }
+            });
+        }
+
+    }
+    public class MeasureModel
+    {
+        public double ElapsedMilliseconds { get; set; }
+        public double Value { get; set; }
+    }
+}
